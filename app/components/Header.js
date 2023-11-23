507e2383df7722a834911d4fffac38a41d3c6596
Charlie <128649583+charlie404-op@users.noreply.github.com>
2023-11-23 12:56:22 +0530

    minor change

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,7 +5,7 @@ import style from "../styles/Header.module.css";
 const Header = () => {
   return (
     <div className={style.wrapper}>
-      <div className={style.title}>SolLotto💰</div>
+      <div className={style.title}>SolLotto </div>
       <button>Connect Wallet</button>
     </div>
   );
